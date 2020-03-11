372af10545c98f0275ec0b81aeb18a52f830b483
Claus Ljunggren <claus@ieee.org>
2020-03-11 09:07:44 +0100

    Update python.py

--- python.py
+++ python.py
@@ -1,3 +1,10 @@
+# Generate Random Strings
 import secrets
 for x in range(15):
-   report('random',secrets.token_hex(16))
+   print(f"random hex string : {secrets.token_hex(16)}")
+
+# Date and Time manipulation
+import datetime
+today_str = '21-02-2020 10:01'
+today = datetime.strptime(today_str, '%d-%m-%Y %H:%M')
+
